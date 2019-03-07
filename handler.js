075bf633b6f96dd4f5b1099093269c3c7eb8c79d
LopezDP <davidplopez@live.com>
2019-03-07 14:04:03 -0500

    refactor: updating handler.js for demo app.

---
+++ handler.js
@@ -0,0 +1,25 @@
+/*
+ * Title: handler.js WebService API
+ * Serverless Starter Service
+ * Author: David P. Lopez
+ * Publisher: UniqueSoftwareDevelopment
+ * March 7, 2019
+ *
+ */
+
+export const hello = async (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `You are now Serverless on AWS! ${(await message({ time: 1, copy: "Your serverless lambda executed successfully!"}))}`,
+      input: event
+    })
+  };
+
+  callback(null, response);
+};
+
+const message = ({time, ...rest}) => new Promise((resolve, reject) => setTimeout(() => {
+  resolve(`${rest.copy} (with a delay)`);
+}, time * 1000)
+);
